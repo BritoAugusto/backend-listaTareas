a49e9a470ffec3b7bb49bdc65eca96181db9d53c
Augusto Brito <britoaugusto1@gmail.com>
2024-10-11 19:21:16 -0300

    se instaló las dependencias morgan,express,cors y mongoose

---
+++ src/database/databaseConection.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const mongobd = process.env.MONGODB;
+mongoose.connect(mongobd);
+
+const conexion = mongoose.connection;
+conexion.once('open',()=>{
+    console.info('BD conectada')
+})
