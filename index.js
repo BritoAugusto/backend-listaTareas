0bf0c080bbd199740d63e401705f1cd94d5b3423
Augusto Brito <britoaugusto1@gmail.com>
2024-10-11 21:12:18 -0300

    se añadio la conexion a la base de datos mongobd

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import morgan from "morgan";
 import path from 'path';
 import { fileURLToPath } from "url";
+import './src/database/databaseConection.js'
 
 const app = express();
 
